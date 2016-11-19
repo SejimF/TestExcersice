6d71d0b8d63853bd689eea03edb5f827ec90a373
SejimF <kolodkomaxim@gmail.com>
2016-11-19 20:08:48 +0300

    update

--- Test/src/DataBase.java
+++ Test/src/DataBase.java
@@ -2,11 +2,22 @@ public class DataBase
 {
 	
 	UserInput usIn = new UserInput();
-	UserInfo ui = new UserInfo();
 	
-	public void person (){
-		System.out.println("Enter user name: "); ui.setName(usIn.writeString());
-		System.out.println("Enter user job: "); ui.setJob(usIn.writeString());
-		System.out.println("Enter user age: "); ui.setAge(usIn.intnumb());
+	String [] name = new String[10];
+	String [] job = new String[10];
+	int [] age = new int[10];
+	
+	public void NewPerson (){
+		int x = 0;
+		System.out.println("Enter user name: "); name [x] = usIn.writeString();
+		System.out.println("Enter user job: "); job[x] = usIn.writeString();
+		System.out.println("Enter user age: "); age [x] = usIn.intnumb();
+		x++;
+	}
+	
+	public void getPersonName(int a){
+		System.out.println("Name: " + name [a]);
+		System.out.println("Job: " + job[a]);
+		System.out.println("Age: " + age[a]);
 	}
 }
--- Test/src/Main.java
+++ Test/src/Main.java
@@ -4,6 +4,12 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		DataBase db = new DataBase();
+		UserInput uin = new UserInput();
+		
+		db.NewPerson();
+		System.out.println("Enter id: ");
+		db.getPersonName(uin.intnumb());
 		
 		
 	}
